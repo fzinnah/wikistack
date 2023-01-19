9b03531e0c8ca1cdca82e8822ad5d6895f4f5c54
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-01-19 12:30:47 -0800

    before Querying with Models

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const path = require('path');
-const bodyParser = require('body-parser');
-const port = 1234;
+const morgan = require("morgan");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = 1234;
 const layout = require("./views/layout");
+const { db, Page, User } = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
 
-app.get('/', (req, res)=>{
-    res.send(layout(""));
-})
+app.get("/", (req, res) => {
+  res.send(layout(""));
+});
 
+const init = async () => {
+  await db.sync({ force: true });
+  // make sure that you have a PORT constant
+  db.authenticate().then(() => {
+    console.log("connected to the database");
+  });
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
 
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
-})
+init();
