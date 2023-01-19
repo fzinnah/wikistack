94f2200cf27441a4e8955067312e3670b8cb76ec
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-01-19 15:01:01 -0800

    Showing a Wiki Page - Finding the Actual Page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,31 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { addPage } = require("../views");
 const { Page } = require("../models");
 
-
-router.get('/', (req, res, next)=>{
-    res.send('got to GET /wiki/');
+router.get("/", (req, res, next) => {
+  res.send("got to GET /wiki/");
 });
 
-router.post('/', async (req, res, next)=>{
-    try {
-        let data = await req.body;
-        const page = await Page.create({
-          title: data.title,
-          content: data.content
-        });
-        await page.save();
-        console.log('title', req.body.title);
-        console.log('content', req.body.content);
-        // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
-        res.redirect('/');
-      } catch (error) { next(error) }
+router.post("/", async (req, res, next) => {
+  try {
+    let data = await req.body;
+    const page = await Page.create({
+      title: data.title,
+      content: data.content,
+    });
+    await page.save();
+    console.log("title", req.body.title);
+    console.log("content", req.body.content);
+    // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/add", (req, res) => {
-    console.log('req body', req.body);
-    res.send(addPage());
+  console.log("req body", req.body);
+  res.send(addPage());
+});
+
+router.get("/:slug", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug,
+      },
+    });
+    res.json(page);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
